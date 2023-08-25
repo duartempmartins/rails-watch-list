b48bf4f38ea52df3082c82b70e709d647f83cbe4
Duarte Martins <duartempmartins@gmail.com>
2023-08-25 12:07:09 +0100

    refactor and more links

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,11 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:list_id])
     @bookmark.list = @list
     @bookmark.save
     if @bookmark.save
@@ -26,5 +25,9 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
